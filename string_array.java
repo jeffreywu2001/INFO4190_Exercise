19c055f23c8062ec8b8b42dad906b682e95bcee9
Jeffrey Wu <97930128+jeffreywu2001@users.noreply.github.com>
2024-04-06 13:26:54 -0700

    Create string_array.java

---
+++ string_array.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SortStrArray {
+    public static void main(String[] args) {
+        String[] strArray = {"orange", "donut", "sushi", "apple", "pizza"};
+        System.out.println("Original String Array: " + Arrays.toString(strArray));
+        SortableArray<String> sortableStrArray = new SortableArray<>(strArray);
+        sortableStrArray.sort();
+        System.out.println("Sorted String Array: " + Arrays.toString(sortableStrArray.toArray()));
+    }
+
+    static class SortableArray<T extends Comparable<T>> {
+        private T[] array;
+
+        public SortableArray(T[] array) {
+            this.array = array;
+        }
+
+        public void sort() {
+            Arrays.sort(array);
+        }
+
+        public T[] toArray() {
+            return array;
+        }
+    }
+}
